77ed4b6c61f474647d0ff7b8710fb076cbb61013
Yassh2003 <yashpratapindia789@gmail.com>
2024-02-03 18:50:52 +0530

    Added Dark mode

--- CryptoApp/src/main.jsx
+++ CryptoApp/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {ChakraProvider, theme} from "@chakra-ui/react"
+import ColorModeSwitcher from './Component/ColorModeSwitcher.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider theme = {theme}>
+      <ColorModeSwitcher/>
       <App />
     </ChakraProvider>
   </React.StrictMode>
