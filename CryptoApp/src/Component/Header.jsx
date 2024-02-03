42d9b9834996d66f4a3b4e8623d847d478c3278d
Yassh2003 <yashpratapindia789@gmail.com>
2024-02-03 19:54:07 +0530

    Made Nav bar sticky

--- CryptoApp/src/Component/Header.jsx
+++ CryptoApp/src/Component/Header.jsx
@@ -1,10 +1,10 @@
-import { Button, HStack } from '@chakra-ui/react'
+import { Button, Container, HStack } from '@chakra-ui/react'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <HStack p= {"4"} shadow={"base"} bgColor={"blackAlpha.900"}>
+    <HStack p= {"4"} shadow={"base"} bgColor={"blackAlpha.900"} pos={"sticky"} top="0" left="0">
       <Button variant={"unstyled"} color={"white"} marginRight={"8"}>
         <Link to="/">Home</Link>
       </Button>
